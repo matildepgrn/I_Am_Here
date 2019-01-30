c6ca4e5daa981a584a283a6710cb104c8d67b286
Matilde Nascimento <matilde.nascimento@tecnico.ulisboa.pt>
2019-01-30 16:23:39 +0000

    Add CheckAttendance function

--- database/schema.sql
+++ database/schema.sql
@@ -1,5 +1,6 @@
 use ist182083;
 
+DROP FUNCTION IF EXISTS CheckAttendance;
 drop PROCEDURE if exists AttendanceMapping;
 drop table if exists Evaluation;
 drop table if exists Code;
@@ -66,7 +67,6 @@ CREATE TABLE Attendance (
 	code_length			int,
 	total_time_s		int,
 	consecutive_codes	int,
-	classnumber			int,
 	open				boolean,
 
 	PRIMARY KEY(attendanceID),
@@ -120,11 +120,51 @@ CREATE TABLE Evaluation (
 );
 
 DELIMITER //
-CREATE PROCEDURE AttendanceMapping (randomID int, code_type varchar(255), code_length int, total_time_s int, consecutive_codes int, classnumber int)
+CREATE PROCEDURE AttendanceMapping (randomID int, code_type varchar(255), code_length int, total_time_s int, consecutive_codes int)
 BEGIN
-INSERT INTO Attendance(randomID, code_type, code_length, total_time_s, consecutive_codes, classnumber)
-		VALUES(randomID, code_type, code_length, total_time_s, consecutive_codes, classnumber);
+INSERT INTO Attendance(randomID, code_type, code_length, total_time_s, consecutive_codes)
+		VALUES(randomID, code_type, code_length, total_time_s, consecutive_codes);
 SELECT LAST_INSERT_ID() AS attendanceID;
+END
+//
+DELIMITER ;
+
+
+DELIMITER //
+CREATE FUNCTION CheckAttendance(attendanceID int, ist_id varchar(255), consecutive_codes int)
+RETURNS boolean
+BEGIN
+DECLARE row_sequence INTEGER;
+DECLARE row_correct boolean;
+DECLARE count INTEGER DEFAULT 0;
+DECLARE finished INTEGER DEFAULT 0;
+DECLARE consecutiveTrue CURSOR for
+	SELECT correct, sequence
+	FROM Code
+		WHERE attendanceID = attendanceID AND ist_id = ist_id
+	ORDER BY sequence;
+
+DECLARE CONTINUE HANDLER
+FOR NOT FOUND SET finished = 1;
+
+OPEN consecutiveTrue;
+myLoop: LOOP
+	FETCH consecutiveTrue INTO row_sequence, row_correct;
+    IF finished = 1 THEN
+		LEAVE myLoop; 
+	END IF;
+    IF row_correct = 1 THEN
+		SET count = count + 1;
+        IF count = consecutive_codes THEN
+			CLOSE consecutiveTrue;
+			RETURN true;
+        END IF;
+	ELSE
+		SET count = 0;
+	END IF;
+END LOOP myLoop;
+CLOSE consecutiveTrue;
+RETURN false;
 END
 //
 DELIMITER ;
