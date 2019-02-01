23b80be6403e864abd1bbfa06ede2e52a8462494
Matilde Nascimento <matilde.nascimento@tecnico.ulisboa.pt>
2019-02-01 14:41:25 +0000

    Create ProfessorTeachesCourse table in db

--- database/schema.sql
+++ database/schema.sql
@@ -15,11 +15,11 @@ drop table if exists User;
 
 CREATE TABLE User (
 	ist_id						varchar(255),
-	access_token 			varchar(255),
-	refresh_token 			varchar(255),
-	iamhere_token		varchar(255),
+	access_token 				varchar(255),
+	refresh_token 				varchar(255),
+	iamhere_token				varchar(255),
 	name						varchar(255),
-	role							varchar(255),
+	role						varchar(255),
 	creation					timestamp,
 
 	PRIMARY KEY(ist_id)
@@ -33,27 +33,34 @@ CREATE TABLE Professor (
 
 CREATE TABLE Course (
 	courseID					varchar(255),
-	courseName			varchar(255),
-	campus					varchar(255),
+	courseName					varchar(255),
 
 	PRIMARY KEY(courseID)
 );
 
+CREATE TABLE ProfessorTeachesCourse (
+	ist_id 						varchar(255),
+	courseID					varchar(255),
+
+	FOREIGN KEY(ist_id) REFERENCES Professor,
+	FOREIGN KEY(courseID) REFERENCES Course
+);
+
 CREATE TABLE Class (
 	courseID						varchar(255),
-	classID						varchar(255),
-	nr_student_enrolled	int,
-	schedule					varchar(255),
+	classID							varchar(255),
+	nr_student_enrolled				int,
+	schedule						varchar(255),
 
 	PRIMARY KEY(classID),
 	FOREIGN KEY(courseID) REFERENCES Course(courseID)
 );
 
 CREATE TABLE Schedule (
-	scheduleID					varchar(255),
-	classID						varchar(255),
-	nr_attendance				int,
-	date_time					timestamp,
+	scheduleID						varchar(255),
+	classID							varchar(255),
+	nr_attendance					int,
+	date_time						timestamp,
 
 	PRIMARY KEY(scheduleID),
 	FOREIGN KEY(classID) REFERENCES Class(classID)
@@ -64,27 +71,27 @@ CREATE TABLE Attendance (
 	randomID						int,
 	scheduleID						varchar(255),
 	code_type						varchar(255),
-	code_length					int,
-	total_time_s						int,
-	consecutive_codes			int,
-	open								boolean,
+	code_length						int,
+	total_time_s					int,
+	consecutive_codes				int,
+	open							boolean,
 
 	PRIMARY KEY(attendanceID),
 	FOREIGN KEY(scheduleID) REFERENCES Schedule(scheduleID)
 );
 
 CREATE TABLE CodeAttendance (
-	server_code			VARCHAR(255),
-	attendanceID			int,
-	sequence				int,
+	server_code						VARCHAR(255),
+	attendanceID					int,
+	sequence						int,
 
 	PRIMARY KEY(attendanceID, sequence),
 	FOREIGN KEY(attendanceID) REFERENCES Attendance(attendanceID)
 );
 
 CREATE TABLE Fingerprint (
-	fingerprintID			int AUTO_INCREMENT,
-	attendanceID 			int,
+	fingerprintID				int AUTO_INCREMENT,
+	attendanceID 				int,
 	ist_id						varchar(255),
 
 	PRIMARY KEY(fingerprintID),
@@ -94,13 +101,13 @@ CREATE TABLE Fingerprint (
 
 CREATE TABLE Code (
 	codeID					int AUTO_INCREMENT,
-	date_input			timestamp,
+	date_input				timestamp,
 	ist_id 					varchar(255),
-	attendanceID 		int,
+	attendanceID 			int,
 	correct 				boolean,
-	sequence			int,
-	code_input 			varchar(255),
-	time_taken_s 		int,
+	sequence				int,
+	code_input 				varchar(255),
+	time_taken_s 			int,
 
 	PRIMARY KEY(codeID),
 	FOREIGN KEY(ist_id) REFERENCES User(ist_id),
@@ -108,9 +115,9 @@ CREATE TABLE Code (
 );
 
 CREATE TABLE AttendanceHistory (
-	attendancehistoryID	int auto_INCREMENT,
+	attendancehistoryID			int auto_INCREMENT,
     randomID					int,
-    ist_id							varchar(255),
+    ist_id						varchar(255),
 	success						boolean,
     
     PRIMARY KEY(attendancehistoryID),
@@ -120,10 +127,10 @@ CREATE TABLE AttendanceHistory (
 
 CREATE TABLE Evaluation (
 	evaluation_id			int AUTO_INCREMENT,
-	ist_id 						varchar(255),
+	ist_id 					varchar(255),
 	attendanceID 			int,
-	nr_clicks					int,
-	session_time_s		int,
+	nr_clicks				int,
+	session_time_s			int,
 
 	PRIMARY KEY(evaluation_id),
 	FOREIGN KEY(ist_id) REFERENCES User(ist_id),
